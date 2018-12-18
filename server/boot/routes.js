c39add1201698e3855aa412c53f533ba54d371ab
M8-Desktop <carloscba1@gmail.com>
2018-12-18 12:45:49 -0300

    - Login middleware

---
+++ server/boot/routes.js
@@ -0,0 +1,18 @@
+var app = require('../server');
+
+app.post('/api/Users/login', function (req, res) {
+
+    app.models.User.login({
+        email: req.body.email,
+        password: req.body.password
+    }, 'user', function (err, token) {
+        if (err) {
+            res.send(err);
+            return;
+        }
+
+        res.send({
+            id: token.id
+        });
+    });
+});
